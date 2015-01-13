5bc07fb5549bca333f66fbafd6271a7cd2c667f3
James Hollowell <jamesthollowell@gmail.com>
2015-01-12 23:15:18 -0500

    Make the mod loadgit status! Stupid FML manifest stuff options -_-

--- src/main/java/allout58/mods/nbttrack/NBTTrackMod.java
+++ src/main/java/allout58/mods/nbttrack/NBTTrackMod.java
@@ -7,9 +7,12 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 /**
  * Created by James Hollowell on 1/11/2015.
  */
-@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = "@VERSION@")
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = "@VERSION@", dependencies = "required-after:OpenMods")
 public class NBTTrackMod
 {
+    @Mod.Instance(ModInfo.MOD_ID)
+    public static NBTTrackMod instance;
+
     @Mod.EventHandler
     public void onServerStartup(FMLServerStartingEvent event)
     {
